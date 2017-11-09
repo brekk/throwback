d923b29f8113a834ed1666f6134a91be5dd0901a
Jon Bullock <jbullock@fanatics.com>
2017-11-08 20:51:20 -0700

    Fix webpack HMR.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 'use strict';
 
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require(`html-webpack-plugin`)
 const path = require('path');
 
 module.exports = {
 
-    entry: './src/index.js',
+  entry: './src/index.js',
 
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'project.bundle.js'
-    },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'project.bundle.js'
+  },
 
-    plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
-    ]
+  plugins: [
+    new webpack.DefinePlugin({
+      'CANVAS_RENDERER': JSON.stringify(true),
+      'WEBGL_RENDERER': JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: `index.html`
+    }),
+  ],
+  devServer: {
+    port: 3030
+  },
+
+  devtool: `inline-source-map`
 
 };

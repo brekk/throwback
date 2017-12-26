93055f53041cf4ce719123cba207f5c3c04e3555
Jon Bullock <jondooger@gmail.com>
2017-12-26 11:31:45 -0600

    Add a triangle model.

--- src/models.js
+++ src/models.js
@@ -1,10 +1,17 @@
 import Phaser from 'phaser'
 
 const {Geom} = Phaser
-const {Circle: PCircle} = Geom
+const {Circle: PCircle, Triangle: PTriangle} = Geom
 // abstract over the original code we wrote and using proper PhaserCircles
 export const Circle = {
   of: ({x, y, a, _radius}) => new PCircle(x, y, _radius || a)
+}
+
+export const Triangle = {
+  equilateral: (x, y, length) => {
+    const height = length * (Math.sqrt(3) / 2)
+    return new PTriangle(x, y, x + length / 2, y + height, x - length / 2, y + height)
+  }
 }
 
 export const Bullet = {

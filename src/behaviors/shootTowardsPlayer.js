cdacabf70f332360960e40a984b05a6ef8d70601
Jon Bullock <jondooger@gmail.com>
2017-12-28 16:46:33 -0600

    Remove unused log message.

--- src/behaviors/shootTowardsPlayer.js
+++ src/behaviors/shootTowardsPlayer.js
@@ -5,7 +5,6 @@ import {Vector} from '../util'
 export const shootTowardsPlayer = (fireFromPosition, size, speed) => {
   const {player} = WORLD
   const targetVector = Vector.fromTo(fireFromPosition, player.properties.position())
-  console.log(targetVector)
   const {x, y} = fireFromPosition
   const b = Bullet.of({x, y, radius: size, vector: targetVector, speed, owner: `enemy`})
   WORLD.ephemera.bullets.push(b)
